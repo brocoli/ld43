f0ef18dbf235af31d1af73960afb5486537ab28e
Julio 'brocoli' Felipe Angelini <julio.angelini@gmail.com>
2018-12-01 19:44:43 -0200

    Work around Godot crash

--- scene/Card.gd
+++ scene/Card.gd
@@ -3,4 +3,12 @@ extends RigidBody2D
 func _on_Card_body_entered(body):
 	var trashcan = get_node("/root/Main/Trashcan")
 	if body == trashcan:
+		cancel_touches()
 		self.queue_free()
+
+func cancel_touches():
+	var event = InputEventMouseButton.new()
+	event.button_index=BUTTON_LEFT
+	event.position = position
+	event.pressed = false
+	get_tree().input_event(event)
