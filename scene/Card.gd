15b02bf9a713225d1bdaf286fd04d8618e0ce4e2
Julio 'brocoli' Felipe Angelini <julio.angelini@gmail.com>
2018-12-01 11:55:05 -0200

    Add empty main scene

--- scene/Card.gd
+++ scene/Card.gd
@@ -3,6 +3,11 @@ signal hit
 
 var lifted = false
 
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton and not event.pressed:
 		lifted = false
