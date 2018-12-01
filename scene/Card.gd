6107b13f83e8548be6ecfce00db9cc98ce2245ea
Julio 'brocoli' Felipe Angelini <julio.angelini@gmail.com>
2018-12-01 13:19:34 -0200

    Simplify joint creation and make it possible for the card to spin

--- scene/Card.gd
+++ scene/Card.gd
@@ -4,36 +4,25 @@ signal hit
 export (PackedScene) var TouchDragPhysics
 
 var draggableBody = null
-var dragJoint = null
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton and not event.pressed:
+	if event is InputEventMouseButton and not event.pressed and draggableBody != null:
 		drop()
 	if (draggableBody != null) and event is InputEventMouseMotion:
 		draggableBody.position += event.relative
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and draggableBody == null:
 		lift()
 
 func lift():
 	draggableBody = TouchDragPhysics.instance()
-	draggableBody.position = self.position
+	draggableBody.position = get_viewport().get_mouse_position()
 	get_parent().add_child(draggableBody)
 	
-	dragJoint = PinJoint2D.new()
-	dragJoint.set_node_a(draggableBody.get_path())
-	dragJoint.set_node_b(get_path())
-	get_parent().add_child(dragJoint)
+	var joint = draggableBody.get_node("PinJoint2D")
+	joint.set_node_b(get_path())
 
 func drop():
-	dragJoint.queue_free()
-	dragJoint = null
-	
 	get_parent().remove_child(draggableBody)
 	draggableBody = null
